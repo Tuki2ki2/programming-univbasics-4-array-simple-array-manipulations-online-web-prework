036dbb1e19d14eea11fc143f5c59cf84c8d4940c
Erik Prenzlin <eprenzlin@gmail.com>
2020-01-01 18:21:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -52,21 +52,7 @@ list_of_programming_languages [0, "Python"]
 
 list_of_programming_languages.insert (list_of_programming_languages)
 
-describe "using_insert" do 
-  before(:each) do 
-    @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-    @another_language = "Python"
-    @new_array = using_insert(@list_of_programming_languages, @another_language)
-  end
 
-  it "takes in two arguments, an array and a new element to be added to the array. it uses the insert method to add the new element to the 4th index of the array" do 
-    expect(@new_array[4]).to eq(@another_language)
-  end
-
-  it "increases the length of the array" do 
-    expect(@new_array.length).to eq(8)
-  end
-end
 
 describe "using_uniq" do 
   it "takes in an argument of an array and uses the uniq method to remove any duplicate items" do
